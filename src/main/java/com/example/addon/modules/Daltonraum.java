4e2149836dac3eae0bd921254b137765e751cf6b
Marccccccccccccccc <mako2010@outlook.de>
2025-10-02 22:55:33 +0200

    Third Push

--- src/main/java/com/example/addon/modules/Daltonraum.java
+++ src/main/java/com/example/addon/modules/Daltonraum.java
@@ -1,11 +1,30 @@
 package com.example.addon.modules;
 
 import com.example.addon.DaltonUtils;
-import meteordevelopment.meteorclient.systems.modules.Category;
+import java.util.ArrayList;
+import java.util.List;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.movement.Scaffold;
+import meteordevelopment.meteorclient.utils.player.FindItemResult;
+import meteordevelopment.meteorclient.utils.player.InvUtils;
+import meteordevelopment.meteorclient.utils.world.BlockUtils;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 
 public class Daltonraum extends Module {
     public Daltonraum() {
         super(DaltonUtils.CATEGORY, "Daltonraum", "description");
+    }
+
+    ///double roundedX = Math.round(mc.player.getX());
+    ///double roundedY = Math.round(mc.player.getY());
+    ///double roundedZ = Math.round(mc.player.getZ());
+
+
+    @Override
+    public void onActivate() {
+
+
+        );
     }
 }
