ace7042cf8fe950add60322fc021fee505d6567b
Marccccccccccccccc <mako2010@outlook.de>
2025-10-02 17:04:43 +0200

    Second Push

--- src/main/java/com/example/addon/DaltonUtils.java
+++ src/main/java/com/example/addon/DaltonUtils.java
@@ -13,14 +13,14 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
-public class AddonTemplate extends MeteorAddon {
+public class DaltonUtils extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
+    public static final Category CATEGORY = new Category("DaltonUtils");
     public static final HudGroup HUD_GROUP = new HudGroup("Example");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Meteor Addon DaltonUtils");
 
         // Modules
         Modules.get().add(new ModuleExample());
---
+++ src/main/java/com/example/addon/modules/Daltonraum.java
@@ -0,0 +1,11 @@
+package com.example.addon.modules;
+
+import com.example.addon.DaltonUtils;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Daltonraum extends Module {
+    public Daltonraum() {
+        super(DaltonUtils.CATEGORY, "Daltonraum", "description");
+    }
+}
